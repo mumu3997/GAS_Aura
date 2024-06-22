ef224150dfed6ad698707440877a134eaf3e1198
mumu3997 <mumu3997@gmail.com>
2024-06-22 11:08:07 +0900

    Constructing the ASC and AS

--- Source/Aura/Private/Player/AuraPlayerState.cpp
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -3,9 +3,22 @@
 
 #include "Player/AuraPlayerState.h"
 
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
 AAuraPlayerState::AAuraPlayerState()
 {
+	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+
+	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
+	
 	// How often the server attempts to update the clientHow often the server attempts to update the client
 	// normally net update frequency is about 0.5sec, it's too slow to update ability system in rpg 
 	NetUpdateFrequency = 100.f;
+}
+
+UAbilitySystemComponent* AAuraPlayerState::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
 }
